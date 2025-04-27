69e09d99da31622e97eb4824771bc3d957f9e68a
svitlana-h <svitlana.hlz@gmail.com>
2025-04-27 18:34:10 +0400

    Update 01. Data Combining.sql

--- 01. Data Combining.sql
+++ 01. Data Combining.sql
@@ -1,39 +1,39 @@
 -- To combine all monthly datasets into a single table
 
-CREATE TABLE `project-course-428108.cyclistic_dataset.tripdata_combined` AS (
-  SELECT * FROM `project-course-428108.cyclistic_dataset.tripdata_202403`
+CREATE TABLE `cyclistic_dataset.tripdata_combined` AS (
+  SELECT * FROM `cyclistic_dataset.tripdata_202403`
   UNION ALL
-  SELECT * FROM `project-course-428108.cyclistic_dataset.tripdata_202404`
+  SELECT * FROM `cyclistic_dataset.tripdata_202404`
   UNION ALL
-  SELECT * FROM `project-course-428108.cyclistic_dataset.tripdata_202405_part1`
+  SELECT * FROM `cyclistic_dataset.tripdata_202405_part1`
   UNION ALL
-  SELECT * FROM `project-course-428108.cyclistic_dataset.tripdata_202405_part2`
+  SELECT * FROM `cyclistic_dataset.tripdata_202405_part2`
   UNION ALL
-  SELECT * FROM `project-course-428108.cyclistic_dataset.tripdata_202406_part1`
+  SELECT * FROM `cyclistic_dataset.tripdata_202406_part1`
   UNION ALL
-  SELECT * FROM `project-course-428108.cyclistic_dataset.tripdata_202406_part2`
+  SELECT * FROM `cyclistic_dataset.tripdata_202406_part2`
   UNION ALL
-  SELECT * FROM `project-course-428108.cyclistic_dataset.tripdata_202407_part1`
+  SELECT * FROM `cyclistic_dataset.tripdata_202407_part1`
   UNION ALL
-  SELECT * FROM `project-course-428108.cyclistic_dataset.tripdata_202407_part2`
+  SELECT * FROM `cyclistic_dataset.tripdata_202407_part2`
   UNION ALL
-  SELECT * FROM `project-course-428108.cyclistic_dataset.tripdata_202408_part1`
+  SELECT * FROM `cyclistic_dataset.tripdata_202408_part1`
   UNION ALL
-  SELECT * FROM `project-course-428108.cyclistic_dataset.tripdata_202408_part2`
+  SELECT * FROM `cyclistic_dataset.tripdata_202408_part2`
   UNION ALL
-  SELECT * FROM `project-course-428108.cyclistic_dataset.tripdata_202409_part1`
+  SELECT * FROM `cyclistic_dataset.tripdata_202409_part1`
   UNION ALL
-  SELECT * FROM `project-course-428108.cyclistic_dataset.tripdata_202409_part2`
+  SELECT * FROM `cyclistic_dataset.tripdata_202409_part2`
   UNION ALL
-  SELECT * FROM `project-course-428108.cyclistic_dataset.tripdata_202410_part1`
+  SELECT * FROM `cyclistic_dataset.tripdata_202410_part1`
   UNION ALL
-  SELECT * FROM `project-course-428108.cyclistic_dataset.tripdata_202410_part2`
+  SELECT * FROM `cyclistic_dataset.tripdata_202410_part2`
   UNION ALL
-  SELECT * FROM `project-course-428108.cyclistic_dataset.tripdata_202411`
+  SELECT * FROM `cyclistic_dataset.tripdata_202411`
   UNION ALL
-  SELECT * FROM `project-course-428108.cyclistic_dataset.tripdata_202412`
+  SELECT * FROM `cyclistic_dataset.tripdata_202412`
   UNION ALL
-  SELECT * FROM `project-course-428108.cyclistic_dataset.tripdata_202501`
+  SELECT * FROM `cyclistic_dataset.tripdata_202501`
   UNION ALL
-  SELECT * FROM `project-course-428108.cyclistic_dataset.tripdata_202502`
+  SELECT * FROM `cyclistic_dataset.tripdata_202502`
   );
